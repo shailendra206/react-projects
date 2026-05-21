276c05b45aa089a713681c31dc1eb9e1ad755f87
kousei-Git <shailendra700981@gmail.com>
2026-05-22 01:57:55 +0530

    Finally Anime-HTML Done

---
+++ 03-Anime-site/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.basic}>
+      @
+      
+    </div>
+  )
+}
+
+export default App
